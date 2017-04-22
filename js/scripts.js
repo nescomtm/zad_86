de3de14807ee6d29814bcdf2ef8befeb955a8f7f
nescomtm <tomczyk@nescom.co>
2017-04-22 23:05:57 +0200

    zadanie 8.6

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+﻿
+var a = prompt('Podaj Wartość dla : a');
+var b = prompt('Podaj Wartość dla : b');
+var value = 0;
+
+value = (a*a)+(2*a*b)-(b*b);
+
+alert('zmienna a: ' + a + ' zmienna b: ' + b + ' wynik z działania (a*a)+(2*a*b)-(b*b) ' + value);
+
+console.log('zmienna a: ' + a + ' zmienna b: ' + b + ' wynik z działania (a*a)+(2*a*b)-(b*b) ' + value); 
+
+if (value > 0) {
+    alert('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest wieksza od 0 i ma wartosc ' + value);
+    console.log('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest wieksza od 0 i ma wartosc ' + value);
+} else if (value < 0) {
+    alert('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest mniejsza od 0 i ma wartosc ' + value);
+    console.log('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest mniejsza od 0 i ma wartosc ' + value);
+} else {
+    alert('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest równa 0  i ma wartosc ' + value);
+    console.log('zmienna value z działania (a*a)+(2*a*b)-(b*b) jest równa 0  i ma wartosc ' + value);
+}
